1c364eb2d88515fce099cd03b58abd567465a72c
Alexandr <tarasov.alex@gmail.com>
2015-06-19 16:59:54 +0300

    create MainActivity class and layout

---
+++ app/src/main/java/com/tau/remindme/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tau.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Alexandr on 6/19/2015.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
