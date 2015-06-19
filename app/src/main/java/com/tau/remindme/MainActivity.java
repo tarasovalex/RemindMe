e45344fab642807249c00c515471b03bf9c389da
Alexandr <tarasov.alex@gmail.com>
2015-06-19 18:46:35 +0300

    edit toolbar and search button

--- app/src/main/java/com/tau/remindme/MainActivity.java
+++ app/src/main/java/com/tau/remindme/MainActivity.java
@@ -1,15 +1,30 @@
 package com.tau.remindme;
 
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
-/**
- * Created by Alexandr on 6/19/2015.
- */
 public class MainActivity extends Activity {
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+    }
+
+
+    private void initToolbar() {
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
